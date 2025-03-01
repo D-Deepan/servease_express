a3a128c94a75078b5a949c0ad7fad5ff6f20a935
Deepan <deepandhandapani2005@gmail.com>
2025-03-01 22:32:42 +0530

    commit in env

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 //const uri = "mongodb+srv://servease:servease@servease.4nimu.mongodb.net/servease?retryWrites=true&w=majority&appName=servease";
-const uri = "mongodb+srv://deepan:servease@servease.gu3um.mongodb.net/?retryWrites=true&w=majority&appName=Servease";
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(process.env.DATABASE_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
